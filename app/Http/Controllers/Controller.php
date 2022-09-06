d09cd58dfb0556309da289424b60feff5b8ab512
Matheus Gomes <math.s.gomes@gmail.com>
2022-09-06 11:17:18 -0300

    Remove unnecessary controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API Cadastro de Endereços",
+ *      description="API realizada durante o bootcampo de PHP com Laravel da Squadra",
+ * )
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
+ * )
+ * @OA\Tag(
+ *     name="Cadastro de Endereços",
+ *     description="API Endpoints da API de Cadastro de Endereços"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
