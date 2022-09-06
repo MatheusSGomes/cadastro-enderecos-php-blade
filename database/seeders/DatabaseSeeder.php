c184d6d4b22a8a9587049fddeb5832fcf50cbbb9
Matheus Gomes <math.s.gomes@gmail.com>
2022-09-06 12:04:32 -0300

    Add seeders to database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UfSeeder::class);
+        $this->call(MunicipioSeeder::class);
+        $this->call(BairroSeeder::class);
+        $this->call(PessoaSeeder::class);
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
